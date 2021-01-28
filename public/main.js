99b9f1a3bb997d61b4d50f1bf925f074ebb7bf6d
Lucía Cantú-Miller <luciacan2miller@gmail.com>
2021-01-27 22:39:55 -0600

    Moon

--- public/main.js
+++ public/main.js
@@ -1,13 +1,13 @@
+// Create the visualization and put it in our div.
 const viz = new Spacekit.Simulation(document.getElementById('main-container'), {
-  basePath: 'https://typpo.github.io/spacekit/src',
+  basePath: 'https://typpo.github.io/spacekit/src'
 });
 
-// Create a background using Yale Bright Star Catalog data.
 viz.createStars();
 
 viz.createSphere('moon', {
   textureUrl: './moon.jpg',
   debug: {
-    showAxes: true,
+    showAxes: false,
   },
 });
