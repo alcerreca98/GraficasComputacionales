26d275fccdab600d1980baa8564b7947aa12a4c8
Lucía Cantú-Miller <luciacan2miller@gmail.com>
2021-02-02 23:09:27 -0600

    Astronaut

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ renderer.autoClear = false;
 renderer.setClearColor(0x000000, 0.0);
 document.body.appendChild(renderer.domElement);
 
+var mixer;
+var clock = new THREE.Clock();
+
 window.addEventListener('resize', function(){
     var width = window.innerWidth;
     var height = window.innerHeight;
@@ -17,6 +20,7 @@ window.addEventListener('resize', function(){
 
 /**
  * MOON
+ * Moon image from: https://github.com/CoryG89/MoonDemo
  */ 
 var geometry   = new THREE.SphereGeometry(10, 32, 32)
 var material  = new THREE.MeshPhongMaterial()
@@ -26,6 +30,7 @@ scene.add(moonMesh)
 
 /**
  * SPACE
+ * Space Image from: https://images.app.goo.gl/svUg8GqiRxfhDa1H6
  */ 
 const spaceGeometry = new THREE.BoxGeometry( 1000, 1000, 1000 );
 var spaceMaterials = [
@@ -46,57 +51,70 @@ scene.add(space);
 /**
  * LIGHTS & SHADOWS
  */
-const ambientLight = new THREE.AmbientLight( 0x404040 ); // soft white ambientLight
-// scene.add( ambientLight );
+const light1 = new THREE.AmbientLight(0xffffff);
+scene.add(light1);
 
-//Create a DirectionalLight and turn on shadows for the light
-const light = new THREE.DirectionalLight( 0xffffff, 1, 100 );
-light.position.set( 0, 1, 0 ); //default; light shining from top
-light.castShadow = true; // default false
-// scene.add( light );
- 
+const light = new THREE.DirectionalLight(0x404040, 2 * Math.PI);
+light.position.set(0.5, 0, 0.866);
+light.castShadow = true;
+scene.add(light);
+
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
 
 //Set up shadow properties for the light
-light.shadow.mapSize.width = 512; // default
-light.shadow.mapSize.height = 512; // default
-light.shadow.camera.near = 0.5; // default
-light.shadow.camera.far = 500; // default
+light.shadow.mapSize.width = 512;
+light.shadow.mapSize.height = 512;
+light.shadow.camera.near = 0.5;
+light.shadow.camera.far = 500;
 
 /**
  * LOADER
+ * Model reference: https://github.com/YuneVK/slides-threejs
  */
-const loader = new THREE.GLTFLoader();
+var loader = new THREE.GLTFLoader();
+        loader.crossOrigin = true;
+        loader.load("models/astronaut/source/scene.gltf", function(data) {
+          var object = data.scene;
+          object.position.set(-10, 10, 0);
+          object.scale.set(2, 2, 2);
+          object.rotation.set(0, -0.3, 0);
 
-loader.load(
-	'Astronaut.gltf',
-	function ( gltf ) {
-        var obj = gltf.scene
-        obj.castShadow = true; //default is false
-        obj.position.set( -10, 10, 0 );
-        obj.add( ambientLight );
-        obj.add( light );
-		scene.add( gltf.scene );
+          mixer = new THREE.AnimationMixer(data.scene);
 
-	},
-);
+          mixer.clipAction(data.animations[2]).play();
+
+          scene.add(object);
+        });
 
 /**
  * SETUP
  */
 controls = new THREE.OrbitControls(camera, renderer.domElement);
 camera.position.z = 30;
+window.scene = scene;
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.gammaOutput = true;
+
+renderer.physicallyCorrectLights = true;
+renderer.gammaFactor = 2.2;
 
 var render = function(){
     renderer.clear();
     moonMesh.rotation.y += 0.001;
-    //spaceShip.rotation.y += .01;
     renderer.render(scene, camera);
 };
 
 var GameLoop = function(){
     requestAnimationFrame(GameLoop);
+        var delta = clock.getDelta();
+
+        if (mixer) {
+          mixer.update(delta);
+        }
+        controls.update();
+        renderer.render(scene, camera);
     render();
 };
 
